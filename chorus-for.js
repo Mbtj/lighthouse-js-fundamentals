1ec185f105f00a3d73826dfc84450c4e4b17bef9
TJ <michael.btj@hotmail.com>
2021-11-05 00:39:29 +0000

    Darude for loop

---
+++ chorus-for.js
@@ -0,0 +1,5 @@
+const chorus = "DUDUDUDUDUDUDUDU"; // now here are some real lyrics to a good classic
+for (let repeat = 0; repeat < 10; repeat++) {
+  console.log(chorus);
+}
+console.log("DUDUDUDUDUDUDUDUDUDUDUDUDU");
